6b825b957acfda07fbb84c6277e3cff29fc3b933
Muhammed Mohsin <156411327+mking1837@users.noreply.github.com>
2024-03-28 12:37:02 +0500

    Backward Counter using python

--- Code/Backward Counter.py
+++ Code/Backward Counter.py
@@ -1,4 +1,9 @@
 for a in range(100,0,-1):
     print(a)
-write("A Backword Counter Using Python")
-write("Made By Muhammed Mohsin")
+import turtle as t
+t.write("A Backword Counter Using Python")
+t.penup()
+t.lt(90)
+t.bk(50)
+t.write("Made By Muhammed Mohsin")
+
