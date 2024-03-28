9b5391c44b6943985205fac42795777421fd51f6
Muhammed Mohsin <156411327+mking1837@users.noreply.github.com>
2024-03-28 12:30:56 +0500

    Tables using user input

--- Code/Tables.py
+++ Code/Tables.py
@@ -1,3 +1,6 @@
 a = int(input("Which Table Do You Want (for eg: 5,2,9)"))
 for count in range(1,11):
     print(a*count)
+
+write("A Backword Counter Using Python")
+write("Made By Muhammed Mohsin")
