d234b78bffce8cef7e3f7cab738f49f63bfc510b
Muhammed Mohsin <156411327+mking1837@users.noreply.github.com>
2024-03-28 12:35:48 +0500

    Tables Using User Input

--- Code/Tables.py
+++ Code/Tables.py
@@ -1,7 +1,11 @@
 
-write("A Backword Counter Using Python")
-write("Made By Muhammed Mohsin")
-
 a = int(input("Which Table Do You Want (for eg: 5,2,9)"))
 for count in range(1,11):
     print(a*count)
+import turtle as t
+t.write("A Backword Counter Using Python")
+t.penup()
+t.lt(90)
+t.bk(50)
+t.write("Made By Muhammed Mohsin")
+
