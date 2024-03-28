e40e69af459cc580d85d78c5b497fe8c7f9f25fa
Muhammed Mohsin <156411327+mking1837@users.noreply.github.com>
2024-03-28 12:02:40 +0500

    Polygon Using Turtle

---
+++ Code/polygon.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.speed(0)
+for p in range(24):
+    t.rt(15)
+    for s in range(36):
+        t.lt(10)
+        t.fd(15)
+
+t.penup()
+t.bd(200)
+t.write('Polygon Using Python Turtle Graphics')
+t.bd(50)
+t.write('Made By Muhammed Mohsin(mdev1836)')
