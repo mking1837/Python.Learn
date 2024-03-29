cc292933890a18396863e91afd7b6e0e05360605
Muhammed Mohsin <156411327+mking1837@users.noreply.github.com>
2024-03-30 02:31:40 +0500

    Hangman using python turtle graphics and random

--- Code/Hangman.py
+++ Code/Hangman.py
@@ -4,7 +4,49 @@ wordlist = ['grape', 'melon', 'chair', 'table', 'brush', 'pasta', 'shirt', 'pant
 word = random.choice(wordlist)
 guess =  '*****'
 
-while(guess != word):
+import random
+import turtle
+turtle.pensize(5)
+errors=0
+def showStickman():
+    if (errors==1):
+        turtle.bk(100)
+        turtle.fd(200)
+    if (errors==2):
+        turtle.bk(100)
+        turtle.lt(90)
+        turtle.fd(300)
+    if (errors==3):
+        turtle.rt(90)
+        turtle.fd(150)
+    if (errors==4):
+        turtle.rt(90)
+        turtle.fd(50)
+    if (errors==5):
+        turtle.dot(50)
+        turtle.fd(100)
+    if(errors==6):
+        turtle.bk(75)
+    if (errors==7):
+        turtle.rt(45)
+        turtle.fd(50)
+        turtle.bk(50)
+    if (errors==8):
+        turtle.lt(90)
+        turtle.fd(50)
+        turtle.bk(50) 
+    if (errors==9):
+        turtle.rt(45)
+        turtle.fd(75)
+        turtle.rt(45)
+        turtle.fd(50)
+        turtle.bk(50)
+    if (errors==10):
+        turtle.lt(90)
+        turtle.fd(50)
+        turtle.penup()
+
+while((guess != word)and(errors<10)):
     print('Here,The Word',guess)
     letter = input('Guess a letter')
     hasletter = False
@@ -23,4 +65,6 @@ while(guess != word):
         print('Goooood Guess')
     else:
         print('Wrong Guess')
+        errors = errors+1
+        showStickman()
 print('The Word Was',word)
